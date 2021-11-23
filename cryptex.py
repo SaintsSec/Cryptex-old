915510228ff8bbaba260621f35d4380c62277b8d
CythesOut <cythesout@gmail.com>
2021-11-23 03:34:33 -0500

    Made it modular

--- cryptex.py
+++ cryptex.py
@@ -4,46 +4,19 @@ TODO Error Checking | IE: If the user fat fingers
 something it will warn that the encryption is not valid
 """
 menuText = """
+  _____              __         
+ / ___/_____ _____  / /______ __
+/ /__/ __/ // / _ \/ __/ -_) \ /
+\___/_/  \_, / .__/\__/\__/_\_\ 
+ V:0.1.3/___/_/ @CythesOut  
     -----{Main menu}-----
-    1.Convert to Hex
-    2.Convert from Hex
-    3.Program info
+    1.Hex Encoder
+    2.Base64 Encoder
+    3.Information
     4.Exit/Quit
     ---------------------
 """
-ans=True
-while ans:
-    #print out menu and ask user what they want to do.
-    print(menuText)
-    ans=input("What would you like to do? ") 
-    if ans=="1": 
-        #Clear the screen and take user to text to hex encoder
-        os.system("clear")
-        print("Text > HEX encoder!\n")
-        #Ask user for text they wish to encode
-        text2hex = input("What would you like to encode:\n")
-        #Convert the text from UTF-8 into hex. 
-        hexIn = text2hex.encode("utf-8").hex()
-        #Print out the hex string
-        print("\nHere is your encoding: \n\n" + hexIn)
-        #TODO Enable | automagic copy of output to clipboard 
-        input("\nPress enter to return to main menu...")
-        os.system("clear")
-    elif ans=="2":
-        #Clear the screen and take user to Hex to Text decoder
-        os.system("clear")
-        print("HEX > Text encoder!\n")
-        #Ask user what they want to convert
-        hex2txt = input("\nWhat would you like to convert:\n")
-        #Decrypt the text and print it out
-        textOut = bytes.fromhex(hex2txt).decode("utf-8")
-        print("\nHere is your encoding: \n\n" + textOut)
-        #TODO Enable | Automagic copy of output into clipboard
-        input("\nPress enter to return to main menu...")
-        os.system("clear")
-    elif ans=="3":
-      os.system("clear")
-      print("""
+information = """
        --------------{Program Information}----------------
       Author: @CythesOut(https://twitter.com/CythesOut)
       Github: https://github.com/CythesOut
@@ -52,7 +25,19 @@ while ans:
       A simple text to hex encoder / decoder. 
       I might do more with it later.
       -----------------------------------------------------
-      """) 
+"""
+ans=True
+while ans:
+    #print out menu and ask user what they want to do.
+    print(menuText)
+    ans=input("What would you like to do? ") 
+    if ans=="1": 
+      exec(open("hex.py").read())
+    elif ans=="2":
+      print("Place holder for BASE64")
+    elif ans=="3":
+      os.system("clear")
+      print(information) 
       input("\nPress enter to return to main menu...")
       os.system("clear")
     elif ans=="4":
---
+++ hex.py
@@ -0,0 +1,62 @@
+import os
+menuText = """
+   __ __                __        _            __
+  / // /____ _____ ____/ /__ ____(_)_ _  ___ _/ /
+ / _  / -_) \ / _ `/ _  / -_) __/ /  ' \/ _ `/ / 
+/_//_/\__/_\_\\_,_/\_,_/\__/\__/_/_/_/_/\_,_/_/
+
+    -----{Main menu}-----
+    1.Encrypt
+    2.Decrypt
+    3.Main Menu
+    4.Exit/Quit
+    ---------------------
+"""
+
+ans = True
+while ans:
+  print(menuText)
+  ans = input("What would you like to do: ")
+  if ans == "1":
+    os.system("clear")
+    print("""
+   ____                       __ 
+  / __/__  __________ _____  / /_
+ / _// _ \/ __/ __/ // / _ \/ __/
+/___/_//_/\__/_/  \_, / .__/\__/ 
+                 /___/_/   
+    """)
+    #Ask user for text they wish to encode
+    text2hex = input("What would you like to encode:\n")
+    #Convert the text from UTF-8 into hex. 
+    hexIn = text2hex.encode("utf-8").hex()
+    #Print out the hex string
+    print("\nHere is your encoding: \n\n" + hexIn)
+    #TODO Enable | automagic copy of output to clipboard 
+    input("\nPress enter to return to main menu...")
+    os.system("clear")
+  elif ans=="2":
+      os.system("clear")
+      print("""
+   ___                        __ 
+  / _ \___ __________ _____  / /_
+ / // / -_) __/ __/ // / _ \/ __/
+/____/\__/\__/_/  \_, / .__/\__/ 
+                 /___/_/ 
+      """)
+      #Ask user what they want to convert
+      hex2txt = input("\nWhat would you like to convert:\n")
+      #Decrypt the text and print it out
+      textOut = bytes.fromhex(hex2txt).decode("utf-8")
+      print("\nHere is your encoding: \n\n" + textOut)
+      #TODO Enable | Automagic copy of output into clipboard
+      input("\nPress enter to return to main menu...")
+      os.system("clear")
+  elif ans == "3":
+    exec(open("cryptex.py").read())
+  elif ans == "4":
+    #exit the script
+    print("\n Goodbye\n\n")
+    exit()   
+  else:
+    print("Invalid option try again!")

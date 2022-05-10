ce970ce2918031308eb7459614fc61351e3ae9ed
Soulsender <soulsender@tutanota.com>
2022-05-10 13:16:45 -0700

    the space jellyfish joins the party - made install.sh look nice, as well as require root

--- install.sh
+++ install.sh
@@ -2,39 +2,52 @@
 # installer for Cryptex
 # created by : C0SM0
 
+# declare variables
+red="\e[0;91m"
+green="\e[0;92m"
+blue="\e[0;94m"
+bold="\e[1m"
+reset="\e[0m"
+
+# check if run with sudo
+if [ "$EUID" -ne 0 ]
+    then echo -e "${red}Please run as root${reset}"
+    exit 0
+fi
+
 # staging
-# echo [*] Staging process...
+# echo -e "${blue}[*] Staging process...${reset}"
 # mkdir ~/.Cryptex
 # cd ..
 # mv Cryptex/* ~/.Cryptex
 # rm -rf Cryptex
 # cd ~/.Cryptex
-# echo [+] Complete
+# echo -e "${green}[+] Complete${reset}"
 
 # staging for testing
-echo [*] Staging process...
+echo -e "${blue}[*] Staging process...${reset}"
 mkdir ~/.Cryptex
 cd ..
 cp Cryptex/* ~/.Cryptex -r
 cd ~/.Cryptex
-echo [+] Completed
+echo -e "${green}[+] Completed${reset}"
 
 #  get tools
-echo [*] Installing tools...
+echo -e "${blue}[*] Installing tools...${reset}"
 sudo apt update
 sudo apt-get install python3
 sudo apt-get install python3-pip python-dev
 pip install qrcode
 pip install Cryptography
-echo [+] Completed
+echo -e "${green}[+] Completed${reset}"
 
 # set up alias workflow
-echo [*] Setting up alias...
+echo -e "${blue}[*] Setting up alias...${reset}"
 echo "alias cryptex=\"python3 $(pwd)/main.py\"" >> ~/.bashrc
 echo "alias cryptex=\"python3 $(pwd)/main.py\"" >> ~/.zshrc
-echo [+] Completed
+echo -e "${green}[+] }Completed${reset}"
 
 # clean up
-echo [+] Installation Completed
-echo "- please restart your terminal"
-echo "- type 'cryptex' launch Cryptex"
+echo -e "${green}[+] Installation Successful"
+echo -e "[+] Please Restart your terminal"
+echo -e "[+] type 'cryptex' launch Cryptex${reset}"

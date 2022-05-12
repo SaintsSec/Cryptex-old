270a00ab247ad90c02ea2861bcca9a4484b18d8c
Blue Cosmo <cosmodiumcs@gmail.com>
2022-05-12 00:44:40 -0400

    DONT-FUCK-WITH-THE-INSTALLER

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # installer for Cryptex
 # created by : C0SM0
+# DO NOT FUCK WITH THIS SCRIPT
 
-# declare variables
+# color variables
 red="\e[0;91m"
 green="\e[0;92m"
 blue="\e[0;94m"
 bold="\e[1m"
 reset="\e[0m"
-
 
 # staging
 # echo -e "${blue}[*] Staging process...${reset}"
@@ -27,7 +27,7 @@ cp Cryptex/* ~/.Cryptex -r
 cd ~/.Cryptex
 echo -e "${green}[+] Completed${reset}"
 
-#  get tools
+# installing tools
 echo -e "${blue}[*] Installing tools...${reset}"
 sudo apt update
 sudo apt-get install python3
@@ -41,22 +41,14 @@ echo -e "${blue}[*] Setting up alias...${reset}"
 
 # check if it already exists in bashrc
 if ! cat ~/.bashrc | grep "CRYPTEX_PATH" > /dev/null; then
-cat << EOF >> ~/.bashrc
-
-# Cryptex alias and path
-export CRYPTEX_PATH="~/.Cryptex"
-alias cryptex="python3 ${CRYPTEX_PATH}/main.py"
-EOF
+echo "export CRYPTEX_PATH=\"~/.Cryptex\"" >> ~/.bashrc
+echo "alias cryptex=\"python3 ${CRYPTEX_PATH}/main.py\"" >> ~/.bashrc
 fi
 
 #check if it already exists in zshrc
-if ! cat ~/.bashrc | grep "CRYPTEX_PATH" > /dev/null; then
-cat << EOF >> ~/.zshrc
-
-# Cryptex alias and path
-export CRYPTEX_PATH="~/.Cryptex"
-alias cryptex="python3 ${CRYPTEX_PATH}/main.py"
-EOF
+if ! cat ~/.zshrc | grep "CRYPTEX_PATH" > /dev/null; then
+echo "export CRYPTEX_PATH=\"~/.Cryptex\"" >> ~/.zshrc
+echo "alias cryptex=\"python3 ${CRYPTEX_PATH}/main.py\"" >> ~/.zshrc
 fi
 
 echo -e "${green}[+] Completed${reset}"

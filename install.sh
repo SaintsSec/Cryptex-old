6ff6380e6944eb9fda116e5b72015513b3917d57
Soulsender <soulsender@tutanota.com>
2022-08-08 12:06:55 -0700

    tweaked install and README docs

--- install.sh
+++ install.sh
@@ -10,6 +10,40 @@ blue="\e[0;94m"
 bold="\e[1m"
 reset="\e[0m"
 
+function staging {
+    # continue prompt
+    while true
+    do
+    read -r -p "This will copy Cryptex to your home directory. If you already had Cryptex installed, this will reinstall it. Would you like to continue? [Y/n]" input
+    case $input in
+        [yY][eE][sS]|[yY])
+        echo -e "${green}Continuing...${reset}"
+        sleep 2
+        install=true
+        break
+        ;;
+
+        [nN][oO]|[nN])
+        echo -e "${red}Operation canceled.${reset}"
+        break
+        exit 0
+        ;;
+
+        *)
+        echo -e "${red}Invalid input...${reset}"
+        ;;
+
+    esac
+    done
+
+    # staging
+    echo -e "${blue}[*] Staging process...${reset}"
+    rm -rf ~/.Cryptex
+    cd ..
+    cp -r Cryptex ~/.Cryptex
+    echo -e "${green}[+] Complete${reset}"
+}
+
 function alias_workflow {
     # set up alias workflow
     echo -e "${blue}[*] Setting up alias...${reset}"
@@ -33,6 +67,9 @@ function alias_workflow {
     fi
 
     echo -e "${green}[+] Completed${reset}"
+
+    # call staging
+    staging
 
     # clean up
     echo -e "${green}[+] Installation Successful"
@@ -63,15 +100,17 @@ case "$1" in
             - Mint
         - Arch
         - Void
+
+        Please see the Cryptex README for more infomation:
+        https://github.com/CryptexProject/Cryptex
   "
   exit 0
 ;;
-
 --unsupported-distro)
     # call alias workflow function
     alias_workflow
+    exit 0
 ;;
-
 esac
 shift
 done
@@ -79,30 +118,12 @@ done
 # check for valid distro (Parrot, Ubuntu, Void, Debian, Arch)
 distro=`sudo cat /etc/issue | awk '{print $1;}'`
 
-# staging
-# echo -e "${blue}[*] Staging process...${reset}"
-# mkdir ~/.Cryptex
-# cd ..
-# mv Cryptex/* ~/.Cryptex
-# rm -rf Cryptex
-# cd ~/.Cryptex
-# echo -e "${green}[+] Complete${reset}"
-
-# staging for testing
-echo -e "${blue}[*] Staging process...${reset}"
-mkdir ~/.Cryptex
-cd ..
-cp Cryptex/* ~/.Cryptex -r
-cd ~/.Cryptex
-echo -e "${green}[+] Completed${reset}"
-
 if [[ "$distro" == "Debian" ]] || [[ "$distro" == "Parrot" ]] || [[ "$distro" == "Ubuntu" ]] || [[ "$distro" == "Linux" ]] || [[ "$distro" == "Kali" ]]; then
     # installing tools for debian
     echo -e "${red}Debian${reset} system detected."
     echo -e "${blue}[*] Installing tools...${reset}"
     sudo apt update
-    sudo apt-get install python3
-    sudo apt-get install python3-pip python-dev
+    sudo apt-get install -y python3 python3-pip python-dev
     pip install qrcode
     pip install Cryptography
     pip install googletrans==3.1.0a0
